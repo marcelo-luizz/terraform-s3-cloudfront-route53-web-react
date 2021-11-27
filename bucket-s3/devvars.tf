22d174c65b2b12ffd25d21c85230c8f2b22f22dd
marcelo-luizz <marcelo.devopseng@gmail.com>
2021-11-27 12:44:16 -0300

    fix

--- bucket-s3/devvars.tf
+++ bucket-s3/devvars.tf
@@ -1,10 +1,3 @@
-# aws_region = "us-east-1"
-  
-# bucket_name = "devopsbrothers.bucket.staging.us-east-1"
-
-# domain = "devopsbrothers.fun"
-
-
 variable "aws_region" {
   type = string
   default = ""
